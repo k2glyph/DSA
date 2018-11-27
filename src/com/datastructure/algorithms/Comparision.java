93af1c450fc0da37642868565966bb0b895c628a
Dinesh katwal <dinesh.katwal@logicabeans.com>
2018-11-27 11:58:20 +0545

    comparision java

--- src/com/datastructure/algorithms/Comparision.java
+++ src/com/datastructure/algorithms/Comparision.java
@@ -1,4 +1,52 @@
 package com.datastructure.algorithms;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class Comparision {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        List<Integer> compList= new ArrayList<>();
+        compList.add(0);
+        compList.add(0);
+        for( int i=0;i<a.size(); i++) {
+            if (a.get(i) > b.get(i)) {
+                compList.set(0, compList.get(0)+1);
+            }
+            if (b.get(i) > a.get(i)) {
+                compList.set(1, compList.get(1)+1);
+            }
+        }
+        return compList;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
